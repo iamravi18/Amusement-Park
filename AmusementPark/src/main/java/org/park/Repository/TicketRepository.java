b3bb77270e4cdc8e373e7eb4e1824354fd4eb225
RAVI KUMAR <88307085+iamravi18@users.noreply.github.com>
2024-06-02 19:12:43 +0530

    Add files via upload

---
+++ AmusementPark/src/main/java/org/park/Repository/TicketRepository.java
@@ -0,0 +1,10 @@
+package org.park.Repository;
+
+import org.park.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findByUserId(Long userId);
+}
---
+++ AmusementPark/src/main/java/org/park/Repository/UserRepository.java
@@ -0,0 +1,8 @@
+package org.park.Repository;
+
+import org.park.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
