6fd73dee126c2b2c67d8055460e069da52f615ec
RAVI KUMAR <88307085+iamravi18@users.noreply.github.com>
2024-06-10 21:45:22 +0530

    Update AmusementParkApplicationTests.java

--- AmusementPark/src/test/java/org/park/AmusementParkApplicationTests.java
+++ AmusementPark/src/test/java/org/park/AmusementParkApplicationTests.java
@@ -3,14 +3,10 @@ package org.park;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest
 class AmusementParkApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertEquals(2,2);
 	}
-
 }
