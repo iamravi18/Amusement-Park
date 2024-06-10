50d765a957279caf10e26e74bcf4fc215c1d1fa3
RAVI KUMAR <88307085+iamravi18@users.noreply.github.com>
2024-06-10 21:41:38 +0530

    Delete AmusementPark/src/main/java/org/park directory

--- AmusementPark/src/main/java/org/park/Repository/ActivityRepository.java
+++
@@ -1,9 +0,0 @@
-package org.park.Repository;
-
-import org.park.model.Activity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActivityRepository extends JpaRepository<Activity, Long> {
-}
--- AmusementPark/src/main/java/org/park/Services/UserService.java
+++
@@ -1,8 +0,0 @@
-package org.park.Services;
-import org.park.model.LoginRequest;
-import org.park.model.User;
-
-public interface UserService {
-    void registerUser(User user);
-    boolean authenticateUser(LoginRequest loginRequest);
-}
